8d10266620796d576288b1864e96b46f63e88c87
Nabi Ayub <nabiaub908@gmail.com>
2026-02-19 02:11:26 +0500

    feat(Films catalogue): added viewAllFilms and addFilm methods

--- src/FilmManager.java
+++ src/FilmManager.java
@@ -1,15 +1,20 @@
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Scanner;
+
 public class FilmManager {
+    Scanner scanner = new Scanner(System.in);
+
     public void showSubMenu()
     {
+        Films.initializeFilms();
         do {
             System.out.println("———————————————————"+"\n\tFilm Catalogue submenu:");
             System.out.println("1. View All Films");
-            System.out.println("2. Add Film");
-            System.out.println("3. Edit Film");
-            System.out.println("4. Delete Film");
-            System.out.println("5. Filter Films");
-            System.out.println("6. Sort Films");
-            System.out.println("7. Back to Main Menu");
+            System.out.println("2. Add Film ");
+            System.out.println("3. Edit Film - not working");
+            System.out.println("4. Delete Film -  not working");
+            System.out.println("5. Sort Films - not working");
+            System.out.println("6. Back to Main Menu");
             System.out.print("———————————————————");
         } while (selectSubMenu());
     }
@@ -18,20 +23,106 @@ public class FilmManager {
         switch (MyUtils.selectChoice(7))
         {
             case 1:
+                viewAllFilms();
                 break;
             case 2:
+                addFilm();
                 break;
             case 3:
+                editFilm();
                 break;
             case 4:
+                deleteFilm();
                 break;
             case 5:
                 break;
             case 6:
-                break;
-            case 7:
                 return false;
         }
         return true;
+    }
+
+    void viewAllFilms()
+    {
+        {
+        System.out.println("———————————————————"+"\nAll Films");
+        if(Films.filmCount == 0)
+        {
+            System.out.println("No films members found.");
+        }
+        else
+        {
+            System.out.println("\nNo. | Name                    | Genre                     | Release date");
+            System.out.println("----+-------------------------+-------------------------+-------------------------");
+            for (int i = 0; i < Films.filmCount; i++) {
+                if(Films.films[i] != null)
+                {
+                    System.out.printf("%-3d | %-23s | %-23s | %-23s\n",
+                            (i + 1), Films.films[i].name, Films.films[i].genre, Films.films[i].dateOfRelease
+                    );
+                }
+            }
+        }
+    }
+    }
+
+    void addFilm()
+    {
+        if (Films.filmCount == 100) {
+            System.out.println("Your films is full");
+        }
+        else
+        {
+            System.out.print("Add film's name (or type X to cancel): ");
+            String newFilmName = scanner.nextLine();
+            if (newFilmName.equalsIgnoreCase("x"))
+            {
+                return;
+            }
+
+            System.out.print("Add film's genre (or type X to cancel): ");
+            String newFilmGenre = scanner.nextLine();
+            if (newFilmName.equalsIgnoreCase("x"))
+            {
+                return;
+            }
+
+            System.out.print("Add film's release date (YYYY-MM-DD) or type X to cancel: ");
+            String newFilmReleaseDate = scanner.nextLine();
+            if (newFilmName.equalsIgnoreCase("x"))
+            {
+                return;
+            }
+
+            System.out.print("Are you sure? (y/n): ");
+            String choiceValidation = scanner.nextLine();
+            if (choiceValidation.equalsIgnoreCase("y") || choiceValidation.equalsIgnoreCase("yes"))
+            {
+                Films.films[Films.filmCount] = new Films(newFilmName, newFilmGenre, newFilmReleaseDate);
+                Films.filmCount++;
+                System.out.println("Film added successfully");
+            }
+            else
+            {
+                System.out.println("Invalid choice or user moved back");
+            }
+
+
+        }
+    }
+
+    void editFilm()
+    {
+        return;
+    }
+
+    void deleteFilm()
+    {
+        return;
+    }
+
+    void sortFilms()
+    {
+        return;
     }
 }
--- src/Films.java
+++ src/Films.java
@@ -1,3 +1,32 @@
 public class Films {
+    String name;
+    String genre;
+    String dateOfRelease;
+
+    static int filmCount = 0;
+
+    static Films[] films = new Films[100];
+
+    public Films(String name, String genre, String dateOfRelease)
+    {
+        this.name = name;
+        this.genre = genre;
+        this.dateOfRelease = dateOfRelease;
+    }
+
+    static public void initializeFilms() {
+    // Just pass the strings in order
+    films[filmCount] = new Films("Interstellar", "Fantasy", "2016-09-03");
+    filmCount++;
+    films[filmCount] = new Films("Lala land", "Romans", "2012-04-03");
+    filmCount++;
+    films[filmCount] = new Films("Pele", "Fantasy", "1998-09-07");
+    filmCount++;
+}
+    void addFilm(String name, String genre, String dateOfRelease)
+    {
+        films[filmCount] = new Films(name=name, genre=genre, dateOfRelease=dateOfRelease);
+        filmCount++;
+    }
 
 }
